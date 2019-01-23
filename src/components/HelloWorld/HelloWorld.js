546d086ea96583173a1aa0bfb73c7f8aaef2b792
Guilherme Porto <guiportooo@gmail.com>
2019-01-23 01:27:53 -0200

    Add documentation components with syntax highlight

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Proptypes from "prop-types";
 
+/** A super lame component that says Hello with a custom message. */
 const HelloWorld = ({ message }) => <div>Hello {message}</div>;
 
 HelloWorld.propTypes = {
+  /** Message to display */
   message: Proptypes.string
 };
 
+HelloWorld.defaultProps = {
+  message: "World"
+};
 export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from "react";
+import HelloWorld from "ps-react-guiportooo/HelloWorld";
+
+/** Custom message */
+const ExampleHelloWorld = () => <HelloWorld message="Pluralsight viewers!" />;
+
+export default ExampleHelloWorld;

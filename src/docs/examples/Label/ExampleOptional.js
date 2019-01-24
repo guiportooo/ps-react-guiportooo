88cf7573be6173ad9c272928cd2594c7154a2d18
Guilherme Porto <guiportooo@gmail.com>
2019-01-24 21:07:37 -0200

    Add Label component

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Label from "ps-react-guiportooo/Label";
+
+const ExampleOptional = () => <Label htmlFor="test" label="test" />;
+
+export default ExampleOptional;
---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Label from "ps-react-guiportooo/Label";
+
+const ExampleRequired = () => <Label htmlFor="test" label="test" required />;
+
+export default ExampleRequired;
